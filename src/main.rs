e9b8d462cb24fbdb0b6801379d5010eb08552557
Patrik Andersson <pandersson@gmail.com>
2023-06-01 12:46:29 +0200

    Adds some more key navigation stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp, fmt, fs, io, path};
+use std::{cmp, fmt::{self, Display}, fs, io, path, ops::Range};
 
 mod elm;
 use elm::Host;
@@ -11,12 +11,12 @@ use tui::RenderingBuffer;
 
 #[derive(Clone, Debug)]
 struct ScreenSize {
-    columns: u16,
-    rows:    u16,
+    columns: usize,
+    rows:    usize,
 }
 
 impl ScreenSize {
-    fn new(columns: u16, rows: u16) -> Self {
+    fn new(columns: usize, rows: usize) -> Self {
         Self { columns, rows }
     }
 
@@ -41,12 +41,13 @@ impl Default for ScreenSize {
 
 impl From<(u16, u16)> for ScreenSize {
     fn from(value: (u16, u16)) -> Self {
-        ScreenSize::new(value.0, value.1)
+        ScreenSize::new(value.0 as usize, value.1 as usize)
     }
 }
 
+#[derive(Debug)]
 struct EditingViewport {
-    line_offset:   usize,
+    row_offset:   usize,
     column_offset: usize,
 }
 
@@ -57,27 +58,51 @@ impl EditingViewport {
         width:      usize, 
         lines:      &'a[String]
     ) -> Option<&'a str> {
-        let effective_line_index = self.line_offset + line_index;
+        let effective_line_index = self.row_offset + line_index;
         if effective_line_index < lines.len() {
             let line = &lines[effective_line_index];
-            let effective_width = cmp::min(width, line.len());
-            let slice = self.column_offset..self.column_offset + effective_width;
-            Some(&line[slice])
+
+            if self.column_offset < line.len() {
+                let len = cmp::min(width, line.len().saturating_sub(self.column_offset));
+                let end = self.column_offset + len;
+                let start = self.column_offset;
+                let slice = start..end;
+                Some(&line[slice])
+            } else if !line.is_empty() {
+                Some(&"«")
+            } else {
+                Some(&"")
+            }
         } else {
             None
         }
+    }
+
+    fn scroll_up(&mut self, by: usize) {
+        self.row_offset = self.row_offset.saturating_sub(by);
+    }
+
+    fn scroll_down(&mut self, by: usize) {
+        self.row_offset += by;
+    }
+
+    fn scroll_left(&mut self, by: usize) {
+        self.column_offset = self.column_offset.saturating_sub(by);
+    }
+
+    fn scroll_right(&mut self, by: usize) {
+        self.column_offset += by;
     }
 }
 
 impl Default for EditingViewport {
     fn default() -> Self {
-        Self { line_offset: Default::default(), column_offset: Default::default() }
+        Self { row_offset: Default::default(), column_offset: Default::default() }
     }
 }
 
 struct EditingModel {
     lines: Vec<String>,
-    viewport: EditingViewport,
 }
 
 impl EditingModel {
@@ -87,15 +112,11 @@ impl EditingModel {
                 "hi, mom".into(),
                 "Hello, world".into(),
             ],
-            viewport: Default::default(),
         }
     }
 
     fn with_lines(lines: &[String]) -> Self {
-        Self {
-            lines:    lines.to_vec(),
-            viewport: Default::default(),
-        }
+        Self { lines: lines.to_vec(), }
     }
 
     fn from_file(file_path: &path::Path) -> io::Result<Self> {
@@ -108,8 +129,8 @@ impl EditingModel {
 
     fn line_count(&self) -> usize { self.lines.len() }
 
-    fn line_at(&self, index: usize, width: usize) -> Option<&str> {
-        self.viewport.select_and_clip(index, width, self.lines.as_slice())
+    fn line_slice(&self, line_index: usize, range: Range<usize>) -> Option<&str> {
+        self.lines.get(line_index).map(|line| &line[range])
     }
 }
 
@@ -119,19 +140,95 @@ impl Default for EditingModel {
     }
 }
 
-struct CursorModel {
-    column: u16, row: u16,
-    screen_size: ScreenSize,
+#[derive(Debug)]
+struct Position {
+    column:      usize,
+    row:         usize,
 }
 
-impl CursorModel {
+impl Position {
+    fn move_up(&mut self, by: usize)    { self.row = self.row.saturating_sub(by)      }
+    fn move_down(&mut self, by: usize)  { self.row += by  /* no! */                            }
+    fn move_left(&mut self)             { self.column = self.column.saturating_sub(1) }
+    fn move_right(&mut self)            { self.column += 1 /* No! */                           }
+}
+
+impl Default for Position {
+    fn default() -> Self {
+        Self { column: Default::default(), row: Default::default() }
+    }
+}
+
+struct NavigationModel {
+    cursor:      Position,
+    screen_size: ScreenSize,
+    viewport:    EditingViewport,
+}
+
+impl NavigationModel {
+    fn is_topmost(&self)    -> bool { self.cursor.row == 0                               }
+    fn is_bottommost(&self) -> bool { self.cursor.row == self.screen_size.rows - 1       }
+    fn is_leftmost(&self)   -> bool { self.cursor.column == 0                            }
+    fn is_rightmost(&self)  -> bool { self.cursor.column == self.screen_size.columns - 1 }
+
+    fn is_recognized(direction: &KeyCode) -> bool {
+        matches!(
+            direction, 
+            KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right |
+            KeyCode::PageUp | KeyCode::PageDown
+        )
+    }
+
     fn move_intended(&mut self, direction: &KeyCode) {
         match direction {
-            KeyCode::Up    => self.row    = self.row.saturating_sub(1),
-            KeyCode::Left  => self.column = self.column.saturating_sub(1),
-            KeyCode::Down  => self.row    += 1,
-            KeyCode::Right => self.column += 1,
-            _              => unimplemented!(),
+            KeyCode::Up    => {
+                if self.is_topmost() {
+                    self.viewport.scroll_up(1)
+                } else {
+                    self.cursor.move_up(1)
+                }
+            }
+
+            KeyCode::Down  => {
+                if self.is_bottommost() {
+                    self.viewport.scroll_down(1)
+                } else {
+                    self.cursor.move_down(1)
+                }
+            }
+
+            KeyCode::Left  => {
+                if self.is_leftmost() {
+                    self.viewport.scroll_left(1)
+                } else {
+                    self.cursor.move_left()
+                }
+            }
+
+            KeyCode::Right => {
+                if self.is_rightmost() {
+                    self.viewport.scroll_right(1)
+                } else {
+                    self.cursor.move_right()
+                }
+            }
+
+            KeyCode::PageUp => {
+                let page = self.screen_size.rows;
+                let scroll_by = page.saturating_sub(self.cursor.row);
+                self.cursor.move_up(page);
+                self.viewport.scroll_up(scroll_by);
+            }
+
+            KeyCode::PageDown => {
+                let page = self.screen_size.rows;
+                let scroll_by = self.cursor.row;
+                self.viewport.scroll_down(scroll_by);
+                let move_by = page.saturating_sub(self.cursor.row);
+                self.cursor.move_down(move_by);
+            }
+
+            _otherwise => unimplemented!(),
         }
     }
 
@@ -141,20 +238,65 @@ impl CursorModel {
     }
 }
 
-impl Default for CursorModel {
+impl Default for NavigationModel {
     fn default() -> Self {
         Self {
-            column: Default::default(), 
-            row: Default::default(),
+            cursor:      Default::default(), 
             screen_size: Default::default(),
+            viewport:    Default::default(),
         }
+    }
+}
+
+struct KeyEvent(event::KeyEvent);
+
+impl From<&event::KeyEvent> for KeyEvent {
+    fn from(event: &event::KeyEvent) -> Self {
+        Self(event.clone())
+    }
+}
+
+impl Display for KeyEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} [{:?}]", self.0.code, self.0.modifiers)
+    }
+}
+
+struct KeyHistory {
+    events: Vec<KeyEvent>,
+    horizon: usize,
+}
+
+impl KeyHistory {
+    fn record(&mut self, event: &event::KeyEvent) {
+        self.events.push(event.into());
+        if self.events.len() > self.horizon {
+            self.events.remove(0);
+        }
+    }
+}
+
+impl Display for KeyHistory {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        for event in &self.events {
+            write!(f, "{},", event)?;
+        }
+        write!(f, "]")
+    }
+}
+
+impl Default for KeyHistory {
+    fn default() -> Self {
+        Self { events: Default::default(), horizon: 3 }
     }
 }
 
 struct Editor {
     buffer_name: String,
     contents:    EditingModel,
-    cursor:      CursorModel,
+    navigation:  NavigationModel,
+    key_history: KeyHistory,
 }
 
 impl Editor {
@@ -168,16 +310,22 @@ impl Editor {
                 elm::Cmd::gtfo(),
 
             event::KeyEvent {
-                code:      direction @ (KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right),
+                code:      direction,
                 modifiers: KeyModifiers::NONE,
                 ..
-            } => {
-                self.cursor.move_intended(direction);
+            } if NavigationModel::is_recognized(direction) => {
+                self.navigation.move_intended(direction);
                 elm::Cmd::none()
             }
 
-            _otherwise => elm::Cmd::none(),
+            ev @ event::KeyEvent { .. } =>
+                self.record_key_event(ev),
         }
+    }
+
+    fn record_key_event(&mut self, ev: &event::KeyEvent) -> elm::Cmd<Message> {
+        self.key_history.record(ev);
+        elm::Cmd::none()
     }
 
     fn event_occurred(&mut self, event: &event::Event) -> elm::Cmd<Message> {
@@ -185,14 +333,14 @@ impl Editor {
             event::Event::Key(key) =>
                 self.key_typed(key),
             event::Event::Resize(width, height) =>
-                self.cursor.screen_size_changed((*width, *height).into()),
+                self.navigation.screen_size_changed((*width, *height).into()),
             _otherwise =>
                 elm::Cmd::none(),
         }
     }
 
     fn render(&self, buffer: &mut RenderingBuffer) -> io::Result<()> {
-        let cursor_bounds = &self.cursor.screen_size;
+        let cursor_bounds = &self.navigation.screen_size;
 
         /* At least consider putting the draw methods behind some
            trait to cut down on the amount of code clutter. */
@@ -203,19 +351,31 @@ impl Editor {
 
         self.render_contents(buffer)?;
 
-        let message = format!("Cursor bounds: {:?}", cursor_bounds);
+        let navigation_message = format!(
+            "size: {:?}, cursor: {:?}, view: {:?}",
+            cursor_bounds,
+            self.navigation.cursor,
+            self.navigation.viewport,
+        );
+
+        let key_message = format!("History: {}", self.key_history);
 
         buffer
             .queue(cursor::MoveTo(5, 10))?
-            .queue(style::Print(message))?
-            .queue(cursor::MoveTo(self.cursor.column, self.cursor.row))?
+            .queue(style::Print(navigation_message))?
+            .queue(cursor::MoveTo(5, 15))?
+            .queue(style::Print(key_message))?
+            .queue(cursor::MoveTo(
+                self.navigation.cursor.column as u16,
+                self.navigation.cursor.row as u16,
+            ))?
             .queue(cursor::Show)?;
 
         Ok(())
     }
 
     fn render_contents(&self, buffer: &mut RenderingBuffer) -> io::Result<()> {
-        let cursor_bounds = &self.cursor.screen_size;
+        let cursor_bounds = &self.navigation.screen_size;
         for i in 0..cursor_bounds.rows  {
             let line = self.render_line(i as usize);
 
@@ -230,20 +390,21 @@ impl Editor {
         Ok(())
     }
 
-    fn render_line(&self, i: usize) -> &str {
-        let line_width = self.cursor.screen_size.columns as usize;
-        self.contents.line_at(i, line_width).unwrap_or("~")
+    fn render_line(&self, viewport_line_index: usize) -> &str {
+        let width = self.navigation.screen_size.columns as usize;
+        self.navigation.viewport
+            .select_and_clip(viewport_line_index, width, &self.contents.lines)
+            .unwrap_or("~")
     }
-
-
 }
 
 impl Default for Editor {
     fn default() -> Self {
         Self {
             buffer_name: "Unnamed".to_owned(),
-            contents:    EditingModel::from_file(path::Path::new("Cargo.toml")).unwrap(),
-            cursor:      CursorModel::default(),
+            contents:    EditingModel::from_file(path::Path::new("src/main.rs")).unwrap(),
+            navigation:  NavigationModel::default(),
+            key_history: Default::default(),
         }
     }
 }
@@ -253,6 +414,10 @@ enum Message {
     SetBufferName(String),
     ExternalEvent(event::Event),
     SizedChanged(ScreenSize),
+}
+
+impl Message {
+    
 }
 
 impl elm::Application for Editor {
@@ -274,7 +439,7 @@ impl elm::Application for Editor {
                 self.event_occurred(event),
 
             Message::SizedChanged(size) =>
-                self.cursor.screen_size_changed(size.clone()),
+                self.navigation.screen_size_changed(size.clone()),
         }
     }
 
